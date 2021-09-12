facb9477bc0bb45de3213994f6e852cec05e8351
wangzhizhou <wangzhizhou@bytedance.com>
2021-09-13 02:48:12 +0800

    FLEX 注入成功

--- OrzInjection/OrzHook/OrzCodeInjection.m
+++ OrzInjection/OrzHook/OrzCodeInjection.m
@@ -11,9 +11,23 @@
 @implementation OrzCodeInjection
 + (void)load {
     NSLog(@"JOKER: 注入成功");
-    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self showFLEX];
+    });
+}
+
++ (void)showFLEX {
+    // 执行: [FLEXManager sharedManager]
     Class FLEX = objc_getClass("FLEXManager");
+    SEL selector1 = NSSelectorFromString(@"sharedManager");
+    IMP imp1 = [FLEX methodForSelector:selector1];
+    id (*func1)(id, SEL) = (void *)imp1;
+    id sharedInstance = func1(FLEX, selector1);
     
-    NSLog(@"JOKER: %@", FLEX);
+    // 执行: [[FLEXManager sharedManager] showExplorer]
+    SEL selector2 = NSSelectorFromString(@"showExplorer");
+    IMP imp2 = [sharedInstance methodForSelector:selector2];
+    void (*func2)(id, SEL) = (void *)imp2;
+    func2(sharedInstance, selector2);
 }
 @end
