f278cd236a2b4c95a1988973802e96ea55733478
wangzhizhou <wangzhizhou@bytedance.com>
2021-09-25 23:29:27 +0800

    添加Clang插桩代码

---
+++ OrzInjection/OrzHook/Clang/OrzClang.h
@@ -0,0 +1,16 @@
+//
+//  OrzClang.h
+//  OrzHook
+//
+//  Created by wangzhizhou on 2021/9/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OrzClang : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
